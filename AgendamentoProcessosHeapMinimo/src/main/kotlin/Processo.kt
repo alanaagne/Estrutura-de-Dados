67444b57399ce8e4cbdcd405e7d7dcf866895c7f
Alana Ágne <alanaagne0@gmail.com>
2024-11-23 09:14:21 -0300

    feat: classe processo

--- AgendamentoProcessosHeapMinimo/src/main/kotlin/Processo.kt
+++ AgendamentoProcessosHeapMinimo/src/main/kotlin/Processo.kt
@@ -1,1 +1,13 @@
-class Processo(val nome: String, var prioridade: Long)
+// Definindo a classe Processo
+class Processo(val nome: String, var prioridade: Long) {
+    // Função que exibe as informações do processo 
+    fun exibirInfo() { 
+        println("Nome do Processo: $nome") 
+        println("Prioridade: $prioridade") 
+        }
+
+    // Função que altera a prioridade do processo 
+    fun alterarPrioridade(novaPrioridade: Long) { 
+        prioridade = novaPrioridade 
+        } 
+    }
